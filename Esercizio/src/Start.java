a66c5684d126eff7c0401c67c47335eb19b8f0bf
Giada Dall'Ara <giada.25700@gmail.com>
2023-01-31 13:51:20 +0100

    correzione esercizio 1 degli array

--- Esercizio/src/Start.java
+++ Esercizio/src/Start.java
@@ -9,12 +9,12 @@ public class Start {
         System.out.println("The 3rd elements of array is: " + carBrands[2]);
 
         int primeNumbers[] = new int[6];
-        primeNumbers[0] = 1;
-        primeNumbers[1] = 2;
-        primeNumbers[2] = 3;
-        primeNumbers[3] = 4;
-        primeNumbers[4] = 5;
-        primeNumbers[5] = 6;
+        primeNumbers[0] = 2;
+        primeNumbers[1] = 3;
+        primeNumbers[2] = 5;
+        primeNumbers[3] = 7;
+        primeNumbers[4] = 11;
+        primeNumbers[5] = 13;
         System.out.println("All the elements of array are: " + Arrays.toString(primeNumbers));
     }
 }
